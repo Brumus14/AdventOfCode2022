0a4e34becbbab6e3638a1299207e29ca4744e1ce
Brumus14 <57995669+Brumus14@users.noreply.github.com>
2022-12-01 13:21:42 +0000

    yes

---
+++ AdventOfCode2022/Day1/Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines = File.ReadAllLines(@"./Input.txt");
+
+            foreach (string line in inputLines)
+	        {
+                if (line == String.Empty)
+                {
+                    inputLines.
+                }
+	        }
+        }
+    }
+}
